f6d22e07100276abf9dac1027534251d5ca11bcc
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-01 22:47:57 +0000

    Add check function to check if a given number is valid

--- luhn-algorithm.js
+++ luhn-algorithm.js
@@ -4,14 +4,36 @@
 - if the sum is a multiple of 10, the account number is possibly valid
 */
 
-function check(number) {
-  var numberArray = number.toString().split("");
-  for(var i = (numberArray.length - 1); i > 0; i-- ) {
-    if(i % 2 !== 0) {
-      numberArray[i] *= 2;
-    }
-  }
-  console.log(numberArray);  // *********************
+// A function that convert a two digit number to a one digit number
+function makeOneDigit(number) {
+  return number - 9;
 }
 
-check(79927398713);
+function isNumberLength_11(array) {
+  return array.length === 11;
+}
+function check(number) {
+  var numberArray = number.toString().split("");
+  var sum = 0;
+
+  // check if a given number has 11 numbers
+  if(!isNumberLength_11(numberArray)) {
+    var message = "given number does not have 11 numbers."
+    return message;
+  }
+
+  for(var i = (numberArray.length - 1); i >= 0; i-- ) {
+    if(i % 2 !== 0) {
+      numberArray[i] *= 2;
+      if(numberArray[i] > 9) {
+        numberArray[i] = makeOneDigit(numberArray[i]);
+      }
+    }
+    sum += Number(numberArray[i]);
+  }
+
+  return sum % 10 === 0;
+}
+
+
+module.exports = check;
